4023f1e67be916155c18c8a8001a1436db170a40
nelson kigen <nellyk89@gmail.com>
2015-02-23 22:06:16 +0100

    Automatically start agents from the command linegit add unix.sh git add unix.sh git add unix.sh git add unix.sh git add unix.sh git add unix.sh

--- scripts/unix.sh
+++ scripts/unix.sh
@@ -1,20 +1,53 @@
 #! /bin/bash
 
 # (2015) nelson kigen <nellyk89@gmail.com>
-# Start the EQR agents from UNIX
+# Start JADE and the EQR agents from UNIX
 
+#set -x
+
+#JADE and SRC path
 JADE_PATH="/home/nkigen/development/jade/jade"
-SRC_PATH="/home/nkigen/development/git/EQR/src/org/nkigen"
+SRC_PATH="/home/nkigen/development/git/EQR"
 
-set -x
+#Main package for the agents
+AB="org.nkigen.eqr.agents" #Agents package with the execption of the simulation agent
+
+#AGENTS CONFIG
+NUM_PATIENT_AGENTS=2
+NUM_FIRE_AGENTS=2
+NUM_HOSPITALS=1
+NUM_AMBULANCES=2
+NUM_FIREENGINES=2
 
 
+#Create all the agents as above including
+#ONE router agent
+#ONE viewer agent
+#ONE updates agent
+#ONE EmergencyControlCenterAgent
+create_agents(){
+#Create Patient Agents
+PATIENTS=""
+for ((i=0; i<=NUM_PATIENT_AGENTS; i++ ));
+do
+	PATIENTS=${PATIENTS}"patient_"${i}":"${AB}".EQRPatientAgent;"
+done
+
+FIRES=""
+for ((i=0; i<=NUM_FIRE_AGENTS; i++ ));
+do
+	FIRES=${FIRES}"fires_"${i}":"${AB}".EQRFireAgent;"
+done
+#echo $FIRES
+#echo $PATIENTS
+export AGENTS="${PATIENTS}${FIRES}simulatorAgent:org.nkigen.eqr.simulation.SimulationAgent;ECC:"${AB}".EmergencyControlCenterAgent"
+
+}
 
 main(){
-	ant -f ${SRC_PATH}/../../../build.xml
-#	java -classpath ${JADE_PATH}/lib/jade.jar jade.Boot -gui &
-	java -cp ${JADE_PATH}/lib/jade.jar; ${SRC_PATH}/../../../bin jade.Boot -gui -agents ping1:examples.PingAgent.PingAgent
-	#javac -classpath lib/jade.jar -d classes ${SRC_PATH}/*.java
+	create_agents
+	ant -f ${SRC_PATH}/build.xml
+	java -cp ${JADE_PATH}/lib/jade.jar:${SRC_PATH}/bin jade.Boot -gui -agents ${AGENTS} 
 
 }
 
